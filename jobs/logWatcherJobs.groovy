a2f12946c475fb334f5ee3f4d7f69b9cbd5e9a7e
Dmytro Khudin <MisterJBee@gmail.com>
2017-01-07 17:54:48 +0200

    Introduce after seed update runner

--- jobs/logWatcherJobs.groovy
+++ jobs/logWatcherJobs.groovy
@@ -1,10 +1,16 @@
+List<String> autoRunOnSeedChangeJobsList = []
+def runOnSeedChange = { String jobName ->
+    autoRunOnSeedChangeJobsList << jobName
+    return jobName
+}
+
 String basePath = 'Logs'
 
 folder(basePath) {
     description 'This folder contains jobs which watch for home logs.'
 }
 
-job("$basePath/Test Log Watcher") {
+job(runOnSeedChange("$basePath/Test Log Watcher")) {
     triggers {
         scm 'H H * * *'
     }
@@ -12,5 +18,15 @@ job("$basePath/Test Log Watcher") {
         shell('''
             echo "Doing nothing, just a test"
         ''')
+    }
+}
+
+job ("Seed Job After Update Runner") {
+    publishers {
+        downstreamParameterized {
+            trigger autoRunOnSeedChangeJobsList, {
+                condition("SUCCESS")
+            }
+        }
     }
 }
