2c4b9d885f876c2f42fed383eb25ad40f43425c7
Dmytro Khudin <MisterJBee@gmail.com>
2017-01-09 15:26:16 +0200

    [UX] Introduce puzzle log watcher

--- jobs/logWatcherJobs.groovy
+++ jobs/logWatcherJobs.groovy
@@ -25,6 +25,22 @@ job(runOnSeedChange("$basePath/Test Log Watcher")) {
     }
 }
 
+job(runOnSeedChange("$basePath/Puzzle Log Watcher")) {
+    def fileName = "/var/log/custom/puzzle/main.log"
+    triggers {
+        cron 'H * * * *'
+    }
+    steps {
+        shell("""
+            ([ -e '$fileName' ] && grep -i -C10 -E "ERROR|exception" '$fileName' || echo "No file found") && (exit 1;)
+        """.trim())
+    }
+
+    publishers {
+        mailer('misterJbee+beehive@gmail.com', true)
+    }
+}
+
 job ("Seed Job After Update Runner") {
     publishers {
         mailer('misterJbee+beehive@gmail.com', true)
